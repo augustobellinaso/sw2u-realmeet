aae0c0d279cf60de9b9007861a5ed8bc349f96c2
Augusto Bellinaso <augustobellinaso@gmail.com>
2021-11-20 15:03:21 -0300

    mais um cenario de teste unitario

--- service/src/test/java/br/com/sw2u/realmeet/unit/RoomServiceUnitTest.java
+++ service/src/test/java/br/com/sw2u/realmeet/unit/RoomServiceUnitTest.java
@@ -4,10 +4,12 @@ import static br.com.sw2u.realmeet.utils.MapperUtils.roomMapper;
 import static br.com.sw2u.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
 import static br.com.sw2u.realmeet.utils.TestDataCreator.newRoomBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import br.com.sw2u.realmeet.core.BaseUnitTest;
 import br.com.sw2u.realmeet.domain.repository.RoomRepository;
+import br.com.sw2u.realmeet.exception.RoomNotFoundException;
 import br.com.sw2u.realmeet.service.RoomService;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,12 +32,18 @@ class RoomServiceUnitTest extends BaseUnitTest {
     }
 
     @Test
-    void testGetRoom() {
+    void testGetRoomSuccess() {
         var room = newRoomBuilder().id(DEFAULT_ROOM_ID).build();
-        when(roomRepository.findById(DEFAULT_ROOM_ID)).thenReturn(Optional.of(room));
+        when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, true)).thenReturn(Optional.of(room));
         var dto = victim.getRoom(DEFAULT_ROOM_ID);
         assertEquals(room.getName(), dto.getName());
         assertEquals(room.getSeats(), dto.getSeats());
         assertEquals(room.getId(), dto.getId());
+    }
+
+    @Test
+    void testGetRoomNotFound() {
+        when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, true)).thenReturn(Optional.empty());
+        assertThrows(RoomNotFoundException.class, () -> victim.getRoom(DEFAULT_ROOM_ID));
     }
 }
