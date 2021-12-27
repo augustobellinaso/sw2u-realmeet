b555c88a3954beeca96a0d3f123db1bd6680a386
Augusto Bellinaso <augustobellinaso@gmail.com>
2021-12-27 09:45:27 -0300

    inserindo a validacao para os campos da allocation

--- service/src/main/java/br/com/sw2u/realmeet/service/AllocationService.java
+++ service/src/main/java/br/com/sw2u/realmeet/service/AllocationService.java
@@ -6,6 +6,7 @@ import br.com.sw2u.realmeet.domain.repository.AllocationRepository;
 import br.com.sw2u.realmeet.domain.repository.RoomRepository;
 import br.com.sw2u.realmeet.exception.RoomNotFoundException;
 import br.com.sw2u.realmeet.mapper.AllocationMapper;
+import br.com.sw2u.realmeet.validator.AllocationValidator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,16 +15,20 @@ public class AllocationService {
     private final AllocationMapper allocationMapper;
     private final RoomRepository roomRepository;
     private final AllocationRepository allocationRepository;
+    private final AllocationValidator allocationValidator;
     
     public AllocationService(AllocationMapper allocationMapper, RoomRepository roomRepository,
-            AllocationRepository allocationRepository) {
+            AllocationRepository allocationRepository, AllocationValidator allocationValidator) {
         this.allocationMapper = allocationMapper;
         this.roomRepository = roomRepository;
         this.allocationRepository = allocationRepository;
+        this.allocationValidator = allocationValidator;
     }
     
     public AllocationDTO createAllocation(CreateAllocationDTO createAllocationDTO) {
         var room = roomRepository.findById(createAllocationDTO.getRoomId()).orElseThrow(() -> new RoomNotFoundException("Room #" + createAllocationDTO.getRoomId() + " not found"));
+        allocationValidator.validate(createAllocationDTO);
+        
         var allocation = allocationMapper.fromCreateAllocationDTOToEntity(createAllocationDTO, room);
         allocationRepository.save(allocation);
         return allocationMapper.fromEntityToAllocationDTO(allocation);
