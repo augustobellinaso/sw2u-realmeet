5f7abd0aa6dd486f7e16318d5e9b6c52f42dd71d
Augusto Bellinaso <augustobellinaso@gmail.com>
2022-01-17 16:09:50 -0300

    configurando as propriedades de envio de email

--- service/src/main/java/br/com/sw2u/realmeet/Application.java
+++ service/src/main/java/br/com/sw2u/realmeet/Application.java
@@ -2,8 +2,10 @@ package br.com.sw2u.realmeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
