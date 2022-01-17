6e9e4b87b93ae56dbc58a4ab19f0c4c5d5945bee
Augusto Bellinaso <augustobellinaso@gmail.com>
2022-01-17 15:55:01 -0300

    corrigindo testes de integração

--- service/src/test/java/br/com/sw2u/realmeet/core/BaseIntegrationTest.java
+++ service/src/test/java/br/com/sw2u/realmeet/core/BaseIntegrationTest.java
@@ -2,34 +2,47 @@ package br.com.sw2u.realmeet.core;
 
 import br.com.sw2u.realmeet.Application;
 import br.com.sw2u.realmeet.api.ApiClient;
-import java.net.MalformedURLException;
-import java.net.URL;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
+import br.com.sw2u.realmeet.domain.entity.Client;
+import br.com.sw2u.realmeet.domain.repository.ClientRepository;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
+
+import static br.com.sw2u.realmeet.utils.TestConstants.TEST_CLIENT_API_KEY;
+import static br.com.sw2u.realmeet.utils.TestConstants.TEST_CLIENT_DESCRIPTION;
+import static org.mockito.BDDMockito.given;
 
 @ActiveProfiles("integration-test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
 public abstract class BaseIntegrationTest {
-
+    
     @Autowired
     private Flyway flyway;
-
+    
     @LocalServerPort
     private int serverPort;
-
+    
+    @MockBean
+    private ClientRepository clientRepository;
+    
     @BeforeEach
     void setup() throws Exception {
         setupFlyway();
+        mockApiKey();
         setupEach();
     }
-
+    
     protected void setupEach() throws Exception {
     }
-
+    
     protected void setLocalhostBasePath(
             ApiClient apiClient,
             String path
@@ -41,10 +54,23 @@ public abstract class BaseIntegrationTest {
                 path
         ).toString());
     }
-
+    
     private void setupFlyway() {
         flyway.clean();
         flyway.migrate();
     }
-
+    
+    private void mockApiKey() {
+        given(clientRepository.findById(TEST_CLIENT_API_KEY)).willReturn(
+                Optional.of(
+                        Client
+                                .newClientBuilder()
+                                .apiKey(TEST_CLIENT_API_KEY)
+                                .description(TEST_CLIENT_DESCRIPTION)
+                                .active(true)
+                                .build()
+                )
+        );
+    }
+    
 }
