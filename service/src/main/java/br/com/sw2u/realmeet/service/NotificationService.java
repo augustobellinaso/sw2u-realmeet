f915a8344eb57a9c0531da7d83edaac1de77e6b6
Augusto Bellinaso <augustobellinaso@gmail.com>
2022-01-21 10:20:42 -0300

    finalizando o notification service

--- service/src/main/java/br/com/sw2u/realmeet/service/NotificationService.java
+++ service/src/main/java/br/com/sw2u/realmeet/service/NotificationService.java
@@ -1,27 +1,42 @@
 package br.com.sw2u.realmeet.service;
 
 import br.com.sw2u.realmeet.domain.entity.Allocation;
+import br.com.sw2u.realmeet.email.EmailInfoBuilder;
 import br.com.sw2u.realmeet.email.EmailSender;
+import br.com.sw2u.realmeet.email.TemplateType;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+import static br.com.sw2u.realmeet.email.TemplateType.*;
 
 @Service
 public class NotificationService {
     
-    private final EmailSender emailSender;
+    private static final String ALLOCATION = "allocation";
     
-    public NotificationService(EmailSender emailSender) {
+    private final EmailSender emailSender;
+    private final EmailInfoBuilder emailInfoBuilder;
+    
+    public NotificationService(EmailSender emailSender, EmailInfoBuilder emailInfoBuilder) {
         this.emailSender = emailSender;
+        this.emailInfoBuilder = emailInfoBuilder;
     }
     
     public void notifyAllocationCreated(Allocation allocation) {
-        //TODO
+        notify(allocation, ALLOCATION_CREATED);
     }
     
     public void notifyAllocationUpdated(Allocation allocation) {
-        //TODO
+        notify(allocation, ALLOCATION_UPDATED);
     }
     
     public void notifyAllocationDeleted(Allocation allocation) {
-        //TODO
+        notify(allocation, ALLOCATION_DELETED);
+    }
+    
+    private void notify(Allocation allocation, TemplateType templateType) {
+        this.emailSender.send(emailInfoBuilder.createEmailInfo(allocation.getEmployee().getEmail(), templateType,
+                                                               Map.of(ALLOCATION, allocation)));
     }
 }
