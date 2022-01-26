28ff3d126dba505bde418d61cbc278f8e0033398
Augusto Bellinaso <augustobellinaso@gmail.com>
2022-01-26 11:49:31 -0300

    criando o resolver para retornar uma instancia de um handler especifico

--- service/src/main/java/br/com/sw2u/realmeet/report/validator/AllocationReportValidator.java
+++ service/src/main/java/br/com/sw2u/realmeet/report/validator/AllocationReportValidator.java
@@ -1,5 +1,8 @@
 package br.com.sw2u.realmeet.report.validator;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AllocationReportValidator extends AbstractReportValidator{
     //TODO
 }
