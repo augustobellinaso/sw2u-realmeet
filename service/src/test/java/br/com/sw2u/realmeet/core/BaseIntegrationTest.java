100c7a23350a93a61767e7adfd786777488dfe95
Augusto Bellinaso <augustobellinaso@gmail.com>
2022-01-29 14:29:02 -0300

    alterando profile para execução de testes

--- service/src/test/java/br/com/sw2u/realmeet/core/BaseIntegrationTest.java
+++ service/src/test/java/br/com/sw2u/realmeet/core/BaseIntegrationTest.java
@@ -20,7 +20,6 @@ import static br.com.sw2u.realmeet.utils.TestConstants.TEST_CLIENT_API_KEY;
 import static br.com.sw2u.realmeet.utils.TestConstants.TEST_CLIENT_DESCRIPTION;
 import static org.mockito.BDDMockito.given;
 
-@ActiveProfiles("integration-test-postgresql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
 public abstract class BaseIntegrationTest {
     
