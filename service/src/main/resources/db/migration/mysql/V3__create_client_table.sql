6f055895c4eca070835e7ad64a6d878177806de2
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-01-29 17:27:27 -0300

    inserindo arquivos db migration

---
+++ service/src/main/resources/db/migration/mysql/V3__create_client_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `client`
+(
+    `api_key`     VARCHAR(20) NOT NULL,
+    `description` VARCHAR(20) NOT NULL,
+    `active`      TINYINT     NOT NULL,
+
+    PRIMARY KEY (`api_key`)
+);
